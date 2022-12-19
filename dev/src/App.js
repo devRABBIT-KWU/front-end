b811615b2c1d04328f271c28ee70c811735ee9d2
rldh1111 <rldh1111@github.com>
2022-12-19 23:37:09 +0900

    package.json

--- dev/src/App.js
+++ dev/src/App.js
@@ -14,48 +14,47 @@ import APP_LOGO from "./image/sample_logo.png";
 import USER_PROFILE_IMAGE from "./image/sample_profile.png";
 
 // NHN - ToastUI Image Editor
-import "tui-image-editor/dist/tui-image-editor.css";
-import ImageEditor from "@toast-ui/react-image-editor";
 
 class App extends Component {
   render() {
     document.documentElement.lang = 'ko';
     return (
-      <div className="App">
-        <div className="AppTitleBar">
-          <div className="AppLogoAndFileName">
-            <img
-              className="AppLogo"
-              src={APP_LOGO}
-              alt="임시 애플리케이션 아이콘"
-            />{" "}
-            SampleImage.png
-          </div>
-          {/* 이 부분을 컴포넌트로 대체할 것 */}
-          <div className="LoginUserInfo">
-            <div id="UserName">데브래빗(devRABBIT)
-              <div id="UserGroup">팀 사용자</div>
+        <div className="App">
+            ImageEditor
+          <div className="AppTitleBar">
+            <div className="AppLogoAndFileName">
+              <img
+                  className="AppLogo"
+                  src={APP_LOGO}
+                  alt="임시 애플리케이션 아이콘"
+              />{" "}
+              SampleImage.png
             </div>
-            
-            <img
-              className="UserProfileImage"
-              src={USER_PROFILE_IMAGE}
-              alt="임시 애플리케이션 아이콘"
-            />
+            {/* 이 부분을 컴포넌트로 대체할 것 */}
+            <div className="LoginUserInfo">
+              <div id="UserName">데브래빗(devRABBIT)
+                <div id="UserGroup">팀 사용자</div>
+              </div>
+
+              <img
+                  className="UserProfileImage"
+                  src={USER_PROFILE_IMAGE}
+                  alt="임시 애플리케이션 아이콘"
+              />
+            </div>
           </div>
-        </div>
 
-        <Menu />
-        <ToolBox />
+          <Menu />
+          <ToolBox />
 
 
-        {/*
+          {/*
         <Menu_top />
         <ToolBox_left />
         <DetailSetting_right />
         */}
-        
-      </div>
+
+        </div>
     );
   }
 }
